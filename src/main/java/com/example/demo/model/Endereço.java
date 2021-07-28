4f92485eb52497b01f3d98e5e70f6d9566491d4b
CarlosEdu1999 <carlosedu19051999@gmail.com>
2021-07-27 21:00:54 -0300

    Created classe endereço

---
+++ src/main/java/com/example/demo/model/Endereço.java
@@ -0,0 +1,70 @@
+package com.example.demo.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.apache.logging.log4j.message.StringFormattedMessage;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name ="endereço")
+public class Endereço {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id ;
+
+    @Column(name="logradouro")
+    private String logradouro;
+
+    @Column(name = "cep")
+    private Integer cep;
+
+    @Column(name = "numero")
+    private Integer numero;
+
+    @Column(name = "cidade")
+    private String cidade;
+
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public Integer getCep() {
+        return cep;
+    }
+
+    public void setCep(Integer cep) {
+        this.cep = cep;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+}
