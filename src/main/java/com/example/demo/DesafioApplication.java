99f7ca4e57284f5a129e0f641941e72a631fdb22
CarlosEdu1999 <carlosedu19051999@gmail.com>
2021-07-28 00:18:04 -0300

    Classe Endereço finalizada

--- src/main/java/com/example/demo/DesafioApplication.java
+++ src/main/java/com/example/demo/DesafioApplication.java
@@ -1,13 +1,24 @@
 package com.example.demo;
 
+import com.example.demo.model.Endereço;
+import com.example.demo.model.Pessoa;
+import com.example.demo.repository.EndereçoRepository;
+import com.example.demo.repository.PessoaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @SpringBootApplication
 public class DesafioApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DesafioApplication.class, args);
+
 	}
+
 
 }
